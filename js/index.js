7c7c0ee663f53863f9e93f9a78a5ac37b3e02d5c
Gehuan Liu <hnnk@qq.com>
2015-10-14 16:57:22 +0800

    添加安卓和ios规则

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
+var bUA = window.navigator.userAgent;
+var deviceType = "";
+if(bUA.indexOf("Android")>=0){
+    deviceType = "Android";
+}else if(bUA.indexOf("iPhone")>=0){
+    deviceType = "iPhone";
+}else if(bUA.indexOf("iPad")>=0){
+	deviceType = "iPad";
+}else{
+	 deviceType = "Android";
+}
+
 jQuery(function($){
-	console.log('999');
+	$(document).click(function(e){
+		console.log(e.target);
+		if($(e.target).is("a")){
+			if(deviceType == "Android"){
+				e.preventDefault();
+				jumpToAndroid("shengyin",$(e.target).data("param")+"");
+				return false;			
+			}else if(deviceType == "iPhone"||deviceType == "iPad"){
+				window.location.href = "bzybedtime://type=shengyin&param="+$(e.target).data("param");
+			}	
+		}
+		
+		
+		
+
+	});
 });
+
+
+window.onload = function(){
+	
+}
