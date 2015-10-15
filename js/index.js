4781190b15b0adc04a6d1c84af093655dfd451b4
Gehuan Liu <hnnk@qq.com>
2015-10-15 11:02:00 +0800

    baidu

--- js/index.js
+++ js/index.js
@@ -12,15 +12,17 @@ if(bUA.indexOf("Android")>=0){
 
 jQuery(function($){
 	
-	$("a").eq(1).attr("href","bzybedtime://type=111&param=222/");
+	$("a").eq(1).attr("href","bzybedtime://type=shengyin&param=943/");
 	
 	$(document).click(function(e){
 		console.log(e.target);
 		if($(e.target).is("a")){
 			if((!!$(e.target).data("type"))){
-				if(deviceType == "Android" && (!!$(e.target).data("param")) ){
+				if(deviceType == "Android"){
 					e.preventDefault();
-					bedtime.jumpToAndroid("shengyin",$(e.target).data("param")+""); //原生安卓webview 接管
+					if(typeof bedtime != "undefined"){
+						bedtime.jumpToAndroid("shengyin",$(e.target).data("param")+""); //原生安卓webview 接管
+					}
 					return false;			
 				}else if(deviceType == "iPhone"||deviceType == "iPad"){
 					
